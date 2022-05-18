36788de24714d6e9b183e420418c5acdda858b19
Jakub Cervinka <jakub.cervinka@zf.com>
2022-05-18 16:13:56 +0200

    simple output to console

--- label_maker.py
+++ label_maker.py
@@ -1,11 +1,12 @@
 from calc import calculate_unit_price
 from inputs import user_input
+from outputs import to_console
 
 
 def main():
     data = user_input()
     calculated_data = calculate_unit_price(data)
-    print(calculated_data)
+    to_console(calculated_data)
 
 
 if __name__ == '__main__':
