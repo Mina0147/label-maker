2422cafdb1c35d8f0b9ce740344e7ff532c45af5
Nguyen Hong Nhung <nhn.nguyen.hong.nhung@gmail.com>
2022-05-24 20:04:57 +0200

    Valid values outside function

--- inputs.py
+++ inputs.py
@@ -1,3 +1,50 @@
+# zkratka: (latinsky, jednotky-pozdeji se vyplní samy),
+VALID_FORMS = {
+    'aer':      ('aerosol',),
+    'cps':      ('capsula',),
+    'cap':      ('capsula',),
+    'col':      ('collyrium',),
+    'crm':      ('cream',),
+    'drg':      ('dragée',),
+    'emp':      ('emplastrum',),
+    'eml':      ('emulsio',),
+    'ext':      ('extractum',),
+    'foa':      ('foam',),
+    'gel':      ('gel',),
+    'glo':      ('globula',),
+    'gra':      ('granulat',),
+    'gtt':      ('guttae',),
+    'inh':      ('inhalatio',),
+    'inj':      ('injectio',),
+    'liq':      ('liquidum',),
+    'lot':      ('lotio',),
+    'ole':      ('oleum',),
+    'plv':      ('pulveres',),
+    'plv ads':  ('pulvis adspersorius',),
+    'pst':      ('pasta',),
+    'shp':      ('shampoo',),
+    'sir':      ('sirupus',),
+    'sol':      ('solutio',),
+    'spc':      ('species',),
+    'spr':      ('spray',),
+    'sup':      ('suppositorium',),
+    'sus':      ('suspensio',),
+    'tab':      ('tabuletta',),
+    'tbl':      ('tabuletta',),
+    'tct':      ('tinctura',),
+    'ung':      ('unguentum',),
+    }
+
+LABEL_ASSETS = {
+        'Name':'Zadejte název produktu: ', 
+        'Form':'Zadejte lékovou formu: ',
+        'Units':'Zadejte typ jednotky: ', 
+        'Quantity':'Zadejte počet jednotek: ',
+        'Price':'Zadejte celkovou cenu: ',
+        }        
+
+VALID_UNITS = ('ml','g','ks',)    # další units...
+
 def user_input():
     """Take input from user.
 
@@ -12,68 +59,25 @@ def user_input():
     seznam = []
     poradi_cedulky = 0
     dalsi = 'a'
-    text = {
-        'Name':'Zadejte název produktu: ', 
-        'Form':'Zadejte lékovou formu: ',
-        'Units':'Zadejte typ jednotky: ', 
-        'Quantity':'Zadejte počet jednotek: ',
-        'Price':'Zadejte celkovou cenu: ',
-        }
-    # zkratka: (latinsky, jednotky-pozdeji se vyplní samy),
-    form = {
-        'aer':      ('aerosol',),
-        'cps':      ('capsula',),
-        'cap':      ('capsula',),
-        'col':      ('collyrium',),
-        'crm':      ('cream',),
-        'drg':      ('dragée',),
-        'emp':      ('emplastrum',),
-        'eml':      ('emulsio',),
-        'ext':      ('extractum',),
-        'foa':      ('foam',),
-        'gel':      ('gel',),
-        'glo':      ('globula',),
-        'gra':      ('granulat',),
-        'gtt':      ('guttae',),
-        'inh':      ('inhalatio',),
-        'inj':      ('injectio',),
-        'liq':      ('liquidum',),
-        'lot':      ('lotio',),
-        'ole':      ('oleum',),
-        'plv':      ('pulveres',),
-        'plv ads':  ('pulvis adspersorius',),
-        'pst':      ('pasta',),
-        'shp':      ('shampoo',),
-        'sir':      ('sirupus',),
-        'sol':      ('solutio',),
-        'spc':      ('species',),
-        'spr':      ('spray',),
-        'sup':      ('suppositorium',),
-        'sus':      ('suspensio',),
-        'tab':      ('tabuletta',),
-        'tbl':      ('tabuletta',),
-        'tct':      ('tinctura',),
-        'ung':      ('unguentum',),
-        }
-    units = ('ml','g','ks',)    # další units...
+    
     while True:   
         if dalsi == 'a':
             seznam.append({})
-            for inpt in text.keys():
-                seznam[poradi_cedulky][inpt] = input(text[inpt])
+            for inpt in LABEL_ASSETS.keys():
+                seznam[poradi_cedulky][inpt] = input(LABEL_ASSETS[inpt])
                 if inpt == 'Quantity' or inpt == 'Price':
                     while True:
                         try:
                             int(seznam[poradi_cedulky][inpt])
                             break
                         except ValueError:
-                            seznam[poradi_cedulky][inpt] = input(text[inpt])
+                            seznam[poradi_cedulky][inpt] = input(LABEL_ASSETS[inpt])
                 elif inpt == 'Units':
-                    while seznam[poradi_cedulky][inpt] not in units:
-                        seznam[poradi_cedulky][inpt] = input(text[inpt])
+                    while seznam[poradi_cedulky][inpt] not in VALID_UNITS:
+                        seznam[poradi_cedulky][inpt] = input(LABEL_ASSETS[inpt])
                 elif inpt == 'Form':  
-                    while seznam[poradi_cedulky][inpt] not in form.keys():
-                        seznam[poradi_cedulky][inpt] = input(text[inpt])
+                    while seznam[poradi_cedulky][inpt] not in VALID_FORMS.keys():
+                        seznam[poradi_cedulky][inpt] = input(LABEL_ASSETS[inpt])
             poradi_cedulky += 1
             dalsi = input('Chcete vytvořit další cedulku? [a/n]: ')
         elif dalsi == 'n':
@@ -81,7 +85,6 @@ def user_input():
         else:
             dalsi = input('Chcete vytvořit další cedulku? [a/n]: ')     
     return seznam
-
 
 
 if __name__ == '__main__':
